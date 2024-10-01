dcf3ba6030c1d98d81535432c1ec11ad63aa07e8
Mazie <f.vafadoost@gmail.com>
2024-09-30 20:20:54 -0700

    adding page not found page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PredictWeather from "./components/PredictWeather";
 import WeatherDetails from "./components/Weather";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./Navigation";
+import NotFound from "./components/NotFound.js";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<WeatherDetails />} />
         <Route path="/predictWeather" element={<PredictWeather />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./NotFound.css";
+export default function NotFound() {
+  return (
+    <div className="main-container">
+      <p className="">Page Not Found</p>
+    </div>
+  );
+}
